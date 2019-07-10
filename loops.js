18320afc9eb09e971742203237fad22a6ab6c544
TonyGomez23 <72.tonygomez@gmail.com>
2019-07-10 10:05:40 -0400

    first comment

---
+++ loops.js
@@ -0,0 +1,13 @@
+let food = ["pizza", "apples", "cookies"];
+
+for(let i = 0; i < 3; i++){
+    console.log(food[i]);
+}
+
+food.push("ice cream" , "sushi");
+
+console.log(food);
+
+for(let i = 0; i < 5; i++){
+    console.log(food[i]);
+}
